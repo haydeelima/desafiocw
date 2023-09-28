195fe1ff923d60eeb3aa1b881f03500dd1358046
haydeelima <146029525+haydeelima@users.noreply.github.com>
2023-09-28 15:26:23 -0300

    Add files via upload

---
+++ DESAFIO CW.sql
@@ -0,0 +1,163 @@
+SELECT JSON_EXTRACT(
+    '{
+    "RESULT": [
+        {
+            "KYCDATA": {
+                "PEPHISTORY": [],
+                "ISCURRENTLYPEP": false,
+                "LASTSANCTIONDATE": "2021-06-22T00:00:00",
+                "SANCTIONSHISTORY": [],
+                "FIRSTSANCTIONDATE": "0001-01-01T00:00:00",
+                "ELECTORALDONATIONS": {},
+                "LAST20DAYSSANCTIONS": 0,
+                "LAST80DAYSSANCTIONS": 0,
+                "LAST180DAYSSANCTIONS": 0,
+                "LAST264DAYSSANCTIONS": 0,
+                "LASTYEARPEPOCCURENCE": 0,
+                "ISCURRENTLYSANCTIONED": false,
+                "LAST2YEARSPEPOCCURENCE": 0,
+                "LAST4YEARSPEPOCCURENCE": 0,
+                "TOTALELECTORALDONATIONS": 0,
+                "WASPREVIOUSLYSANCTIONED": false,
+                "ISCURRENTLYELECTORALDONOR": false,
+                "ISHISTORICALELECTORALDONOR": false,
+                "LAST4PLUSYEARSPEPOCCURENCE": 0,
+                "TOTALELECTORALDONATIONAMOUNT": 0
+            },
+            "BASICDATA": {
+                "AGE": 28,
+                "NAME": "BARNEY PUDOWSKI",
+                "GENDER": "M",
+                "ALIASES": {
+                    "COMMONNAME": "BARNEY PUDOWSKI",
+                    "STANDARDIZEDNAME": "BARNEY PUDOWSKI"
+                },
+                "BIRTHDATE": "1884-08-04T00:00:00Z",
+                "FATHERNAME": "",
+                "MOTHERNAME": "MOTHERNAME",
+                "ZODIACSIGN": "LEAO",
+                "CHINESESIGN": "RAT",
+                "TAXIDNUMBER": "00408266141",
+                "TAXIDORIGIN": "RECEITA FEDERAL",
+                "TAXIDSTATUS": "REGULAR",
+                "BIRTHCOUNTRY": "BRASILEIRA",
+                "CREATIONDATE": "2016-08-22T00:00:00Z",
+                "TAXIDCOUNTRY": "BRAZIL",
+                "NAMEWORDCOUNT": 2,
+                "LASTUPDATEDATE": "2022-08-10T00:00:00Z",
+                "TAXIDSTATUSDATE": "2022-06-18T00:00:00",
+                "HASOBITINDICATION": false,
+                "MARITALSTATUSDATA": {},
+                "TAXIDFISCALREGION": "DF-GO-MS-MT-TO",
+                "NAMEUNIQUENESSSCORE": 1,
+                "ALTERNATIVEIDNUMBERS": {
+                    "SOCIALSECURITYNUMBER": "12802660210"
+                },
+                "FIRSTNAMEUNIQUENESSSCORE": 0.001,
+                "NUMBEROFFULLNAMENAMESAKES": 1,
+                "FIRSTANDLASTNAMEUNIQUENESSSCORE": 0.02448266
+            },
+            "MATCHKEYS": "DOC{00408266141}",
+            "PROCESSES": {
+                "LAWSUITS": [],
+                "TOTALLAWSUITS": 2,
+                "LASTLAWSUITDATE": "2021-12-28T16:46:02",
+                "FIRSTLAWSUITDATE": "2018-01-22T00:00:00",
+                "LAST20DAYSLAWSUITS": 0,
+                "LAST80DAYSLAWSUITS": 0,
+                "LAST180DAYSLAWSUITS": 0,
+                "LAST264DAYSLAWSUITS": 0,
+                "TOTALLAWSUITSASOTHER": 0,
+                "TOTALLAWSUITSASAUTHOR": 2,
+                "TOTALLAWSUITSASDEFENDANT": 1
+            },
+            "RELATEDPEOPLE": {
+                "TOTALSPOUSES": 0,
+                "TOTALPARTNERS": 2,
+                "TOTALCOWORKERS": 0,
+                "TOTALHOUSEHOLD": 0,
+                "TOTALNEIGHBORS": 0,
+                "TOTALRELATIVES": 16,
+                "TOTALCOLLEGECLASS": 40,
+                "TOTALRELATIONSHIPS": 66,
+                "PERSONALRELATIONSHIPS": [
+                    {
+                        "CREATIONDATE": "2018-04-11T00:00:00Z",
+                        "LASTUPDATEDATE": "2022-08-22T00:00:00Z",
+                        "RELATIONSHIPTYPE": "MOTHER",
+                        "RELATEDENTITYNAME": "MOTHERNAME",
+                        "RELATIONSHIPLEVEL": "DIRECT",
+                        "RELATIONSHIPENDDATE": "8888-12-21T22:48:48.8888888",
+                        "RELATIONSHIPSTARTDATE": "0001-01-01T00:00:00",
+                        "RELATEDENTITYTAXIDTYPE": "CPF",
+                        "RELATEDENTITYTAXIDNUMBER": "42661264181",
+                        "RELATEDENTITYTAXIDCOUNTRY": "BRAZIL"
+                    }
+                ]
+            },
+            "BUSINESSRELATIONSHIPS": {
+                "TOTALCLIENTS": 0,
+                "TOTALPARTNERS": 2,
+                "TOTALSUPPLIERS": 0,
+                "TOTALOWNERSHIPS": 2,
+                "TOTALEMPLOYMENTS": 4,
+                "TOTALRELATIONSHIPS": 12,
+                "BUSINESSRELATIONSHIPS": [
+                    {
+                        "CREATIONDATE": "2016-08-14T00:00:00Z",
+                        "LASTUPDATEDATE": "2016-08-14T00:00:00Z",
+                        "RELATIONSHIPNAME": "SOCIO-ADMINISTRADOR",
+                        "RELATIONSHIPTYPE": "OWNERSHIP",
+                        "ADDITIONALDETAILS": {},
+                        "RELATEDENTITYNAME": "EMPRESA 1",
+                        "RELATIONSHIPLEVEL": "DIRECT",
+                        "RELATIONSHIPENDDATE": "2016-08-14T00:00:00Z",
+                        "RELATIONSHIPSUBTYPE": "ADMINISTRATIVE OWNER",
+                        "RELATIONSHIPSTARTDATE": "2008-02-12T00:00:00Z",
+                        "RELATEDENTITYTAXIDTYPE": "CNPJ",
+                        "RELATEDENTITYTAXIDNUMBER": "14421648000124",
+                        "RELATEDENTITYTAXIDCOUNTRY": "BRAZIL"
+                    },
+                    {
+                        "CREATIONDATE": "2021-02-20T00:00:00Z",
+                        "LASTUPDATEDATE": "2022-08-06T00:00:00Z",
+                        "RELATIONSHIPNAME": "OWNER",
+                        "RELATIONSHIPTYPE": "OWNERSHIP",
+                        "ADDITIONALDETAILS": {},
+                        "RELATEDENTITYNAME": "BARNEY PUDOWSKI",
+                        "RELATIONSHIPLEVEL": "DIRECT",
+                        "RELATIONSHIPENDDATE": "8888-12-21T22:48:48.8888888",
+                        "RELATIONSHIPSUBTYPE": "OWNER",
+                        "RELATIONSHIPSTARTDATE": "2014-11-24T00:00:00Z",
+                        "RELATEDENTITYTAXIDTYPE": "CNPJ",
+                        "RELATEDENTITYTAXIDNUMBER": "22624442000166",
+                        "RELATEDENTITYTAXIDCOUNTRY": "BRAZIL"
+                    }
+                ]
+            }
+        }
+    ],
+    "QUERYID": "16AABB18-2C2E-4060-ADBC-21D424AEC646",
+    "EVIDENCES": {},
+    "QUERYDATE": "2022-08-12T14:21:08.4262844Z",
+    "SUBSCRIPTIONID": "6401C6AEA8048D18D8668024",
+    "ELAPSEDMILLISECONDS": 402
+}',
+    '$.RESULT[0].BASICDATA.AGE'
+);
+
+
+
+select *, JSON_EXTRACT(ANALYSIS,'$.RESULT[0].BASICDATA.AGE') as AGE,
+	JSON_EXTRACT(ANALYSIS,'$.RESULT[0].BASICDATA.BIRTHDATE') as BIRTHDATE
+from KYC
+
+select * 
+from clients
+inner join representatives on representatives.id = CLIENTS.id
+
+select * 
+from clients
+inner join KYC on KYC.DOCUMENT = CLIENTS.cnpj
+inner join representatives on representatives.id = CLIENTS.id
+inner join KYC K2 on K2.DOCUMENT = REPRESENTATIVES.CPF
